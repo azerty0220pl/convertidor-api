468fd3b8713bd18818f2c04f83078a14b3ae92da
Szymon Kokot <azerty0220pl@gmail.com>
2023-05-23 15:50:32 +0200

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 this.Schema = mongoose.Schema;
 
