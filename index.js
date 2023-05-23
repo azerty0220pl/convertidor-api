b6b265bd6049188a7a714a59c63bfa2c1372ca15
Szymon Kokot <azerty0220pl@gmail.com>
2023-05-23 13:27:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+this.Schema = mongoose.Schema;
+
+this.entrySchema = new this.Schema({
+    selected: { type: Number, required: true },
+    input: { type: String, required: true }
+});
+
+this.entry = mongoose.model("entry", this.entrySchema);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to database");
+
+    app.get('/load', (req, res) => {
+        this.entry.find().then(entries => {
+            res.send({ saved: entries });
+        });
+    });
+
+    app.post("/save", (req, res) => {
+        new this.entry({ selected: req.body.selected, input: req.body.input }).save().then(() => {
+            res.send({status: "succes"});
+        }).catch(err => {
+            res.send({status: "error"});
+        });
+    });
+
+    app.delete("/delete/:entry", (req, res) => {
+        this.entry.findOneAndRemove(req.entry).then(() => {
+            res.send({status: "succes"});
+        }).catch(err => {
+            res.send({status: "error"});
+        });;
+    });
+})
+
+app.listen(4000, function () {
+    console.log('Listening on port 4000');
+});
